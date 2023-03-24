6f1d1506306645956b37cf8a1095511787e651e0
ShimaZu99 <nikakhubua28@gmail.com>
2023-03-24 13:24:40 -0700

    day1 codes commit

---
+++ day 1/homework.py
@@ -0,0 +1,11 @@
+
+name = "Nika"
+surname = "Khubua"
+age = 19  
+height = 180.5
+words = "Wlis Vswavlob"
+words1 = "Programirebas Goashi"
+words2 = "simaglis"
+print(name + " " +  surname + " " + str(height) + " " + words2 + " " + str(age) + " " +  words + " " + words1)
+
+
---
+++ day 1/lesson1.py
@@ -0,0 +1,29 @@
+#ცვლადები და მათი შექმნა
+
+#name = "nika"
+
+#name არის ცვლადი
+# = არის მნიშვნელობა
+# "nika" არის ცვლადისთვის მნიშვნელობა
+surname = "khubua"
+
+#print(name)
+#Print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nika" #ეს არის (String) ტიპის ცვლადი
+age = 19 #ეს არის (integer) ანუ მთელი რიცხვი
+height = 175.5 #ეს არის (Float) ტიპის ცვლადი ანუ ათ წილადი
+#Boolean (bool) ტიპის ცვლადი
+
+knows_programming = True #ეს არის (bool) ყველა ორ სიტყვიანი ცვლადის შექმნის დროს გამოიყენეთ  _  ქვედა ტირე
+is_ugly = False #Snakecase (უბრალოდ წერის ტიპი სტანდარტულად)
+
+#print(name + " " + surname)            #ასევე არშეიძლება (INT) და (STR) გაერთიანება ( + ) 
+
+#print(type(age))                  #ამ ბრძანებით ჩვენ შეგვიძლია გავიგოთ თუ რომელ ტიპს მიეკუთვნება ესა და ეს ცვლა
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))        #ამ ფუნქციით ჩვენ შეგვიძლია გავსტრინგოთ ინტეჯერები
